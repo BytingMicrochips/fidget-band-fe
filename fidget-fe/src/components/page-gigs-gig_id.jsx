6af61770acee6829ac686aaf790dca84f5fb75ac
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-11-13 17:35:11 +0000

    remove gigContext add useParams for gigs-gig_id

--- fidget-fe/src/components/page-gigs-gig_id.jsx
+++ fidget-fe/src/components/page-gigs-gig_id.jsx
@@ -1,18 +1,28 @@
-import Header from "./header.jsx";
 import gigsData from "../../data/gigs-data.json";
-import { useContext } from "react";
-import { GigContext } from "../App.jsx"; 
+import DrawerNav from "./draw-nav.jsx";
+import Box from "@mui/material/Box";
+import { useParams } from "react-router-dom";
+
 
 const GigsGig_id = () => {
-  const [GigSelected, setGigSelected] = useContext(GigContext);
-  console.log("🚀 ~ file: page-gigs-gig_id.jsx:8 ~ GigSelected:", GigSelected)
+  const params = useParams();
+  const drawerWidth = 240;
 
 
     return (
       <>
-        <Header />
-        <h2> Gigs - Gig_id</h2>
-      
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            padding: 0,
+          }}
+        >
+          <DrawerNav />
+          <h2> Gigs - {params.gig_id}</h2>
+        </Box>
       </>
     );
 };
