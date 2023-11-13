47c31eb0ef154dcc67030444fd3e360d42c73d7c
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2023-11-13 18:16:14 +0000

    add all elements to page-gigs-gig_id

--- fidget-fe/src/components/page-gigs-gig_id.jsx
+++ fidget-fe/src/components/page-gigs-gig_id.jsx
@@ -1,13 +1,22 @@
 import gigsData from "../../data/gigs-data.json";
 import DrawerNav from "./draw-nav.jsx";
 import Box from "@mui/material/Box";
-import { useParams } from "react-router-dom";
-
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const GigsGig_id = () => {
   const params = useParams();
   const drawerWidth = 240;
+  const [thisGig, setThisGig] = useState({});
+  const navigate = useNavigate();
 
+useEffect(() => {
+  gigsData.filter((eachGig) => {
+    if (eachGig.gig_id === params.gig_id) {
+      setThisGig(eachGig)
+    }
+  })
+}, [])
 
     return (
       <>
@@ -21,7 +30,19 @@ const GigsGig_id = () => {
           }}
         >
           <DrawerNav />
-          <h2> Gigs - {params.gig_id}</h2>
+          <h2> {thisGig.title}</h2>
+          <div>
+            <h3>{thisGig.location}</h3>
+            <h3>{thisGig.venue}</h3>
+          </div>
+
+          <h3>{`${new Date(thisGig.date).getDate()}-${new Date(thisGig.date).getMonth()}-${new Date(thisGig.date).getFullYear()}`}</h3>
+          <img src={thisGig.flier} />
+          <h3> {thisGig.description}</h3>
+          <div>
+          <a href={thisGig.ticketLink}>Buy tickets</a>
+            <a onClick={() => { navigate("/gigs") }}>Back to all gigs</a>
+            </div>
         </Box>
       </>
     );
