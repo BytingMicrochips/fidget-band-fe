57bcaad3ee3acbbf2e9f205fdfb74714a17df29a
BytingMicrochips <marcus.bishop.1995@live.co.uk>
2025-07-23 15:10:19 +0100

    add page-gigs-gig_id no longer overspills if gig.description is long add slightly darker background to text for better legibility

--- fidget-fe/src/components/page-gigs-gig_id.jsx
+++ fidget-fe/src/components/page-gigs-gig_id.jsx
@@ -55,7 +55,6 @@ const GigsGig_id = () => {
           flexGrow: 1,
           p: 3,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
-          height: "100vh",
           padding: 0,
         }}
       >
